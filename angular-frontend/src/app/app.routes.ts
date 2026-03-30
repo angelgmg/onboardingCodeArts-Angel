bd70ef84e0ae4afdf490c06c464f730fbd840290
angelgmg <angelgmg7@gmail.com>
2026-03-30 13:53:10 +0200

    Ejercicio 3.4

--- angular-frontend/src/app/app.routes.ts
+++ angular-frontend/src/app/app.routes.ts
@@ -1,9 +1,12 @@
-// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './modules/landing-page-component/landing-page-component';
 import { TasksPageComponent } from './modules/tasks-page-component/tasks-page-component';
+import { LoginComponent } from './modules/auth/login-component/login-component';
+import { RegisterComponent } from './modules/auth/register-component/register-component';
 
 export const routes: Routes = [
   { path: '', component: LandingPageComponent },
   { path: 'tasks', component: TasksPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];

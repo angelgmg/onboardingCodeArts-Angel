1b9b100c4ee411a9707cd62aaa55a84b3eefe474
angelgmg <angelgmg7@gmail.com>
2026-03-31 12:53:14 +0200

    Ejercicio 3.6

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 // src/app/app.component.ts
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastComponent } from './shared/components/toast-component/toast-component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
